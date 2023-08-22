85333c5920ff4522b5daaee06013e90a172f0391
zettacamp-Muhammad-Asyrofuddien <muhammad.asyrofuddien@zettacamp.pro>
2023-08-22 09:17:52 +0700

    Task 1 & 2 Selesai

--- note.js
+++ note.js
@@ -2,7 +2,7 @@ let book1 = 'Koala Kumal';
 const book2 = 'Laskar Pelangi';
 
 book1 = 'Marmut Merah Jambu';
-// book2 = 'Marmut Merah Jambu';
+book2 = 'Marmut Merah Jambu';
 
 let cc = book1 + ' And ' + book2;
 
