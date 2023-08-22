41f88c1ec56deb11e5a73fd5e6572c73952b9bd3
zettacamp-Muhammad-Asyrofuddien <muhammad.asyrofuddien@zettacamp.pro>
2023-08-22 09:06:47 +0700

    Task Javascript Day1

---
+++ task1.js
@@ -0,0 +1,7 @@
+let book1 = 'Koala Kumal';
+const book2 = 'Laskar Pelangi';
+
+book1 = 'Marmut Merah Jambu';
+// book2 = 'Marmut Merah Jambu';
+
+console.log(book1);
---
+++ task2.js
@@ -0,0 +1,9 @@
+let book1 = 'Koala Kumal';
+const book2 = 'Laskar Pelangi';
+
+book1 = 'Marmut Merah Jambu';
+// book2 = 'Marmut Merah Jambu';
+
+let cc = book1 + ' And ' + book2;
+
+console.log(cc);
