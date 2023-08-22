fcc14ae4a6079e51a02df743b3d55ca565d26d0c
zettacamp-Muhammad-Asyrofuddien <muhammad.asyrofuddien@zettacamp.pro>
2023-08-22 09:08:29 +0700

    Update task2

--- task2.js
+++ task2.js
@@ -4,6 +4,6 @@ const book2 = 'Laskar Pelangi';
 book1 = 'Marmut Merah Jambu';
 // book2 = 'Marmut Merah Jambu';
 
-let cc = book1 + ' And ' + book2;
+let cc = book1 + ' An ' + book2;
 
 console.log(cc);
