1af2405cfc9c3ff08c401266a5513fda1a5583e4
zettacamp-Muhammad-Asyrofuddien <muhammad.asyrofuddien@zettacamp.pro>
2023-08-22 10:01:54 +0700

    Finish

--- task2.js
+++ task2.js
@@ -2,8 +2,10 @@ let book1 = 'Koala Kumal';
 const book2 = 'Laskar Pelangi';
 
 book1 = 'Marmut Merah Jambu';
+console.log('Buku Pertama : ' + book1);
+console.log('Buku Kedua   : ' + book1);
 // book2 = 'Marmut Merah Jambu';
 
-let cc = book1 + ' And ' + book2;
+let cc = book1 + ' dan ' + book2;
 
-console.log(cc);
+console.log('Penggabungan : ' + cc);
