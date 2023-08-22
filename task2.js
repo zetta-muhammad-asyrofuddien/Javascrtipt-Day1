0651dac68321265fd66f43ad17d76d4468abd736
zettacamp-Muhammad-Asyrofuddien <muhammad.asyrofuddien@zettacamp.pro>
2023-08-22 10:15:05 +0700

    Update task2.js

--- task2.js
+++ task2.js
@@ -3,7 +3,7 @@ const book2 = 'Laskar Pelangi';
 
 book1 = 'Marmut Merah Jambu';
 console.log('Buku Pertama : ' + book1);
-console.log('Buku Kedua   : ' + book1);
+console.log('Buku Kedua   : ' + book2);
 // book2 = 'Marmut Merah Jambu';
 
 let cc = book1 + ' dan ' + book2;
